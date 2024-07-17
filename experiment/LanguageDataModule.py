e70173bbf234babf32b3569eb4f4aad9f5e85e28
flaitenberger <f.laitenberger@gmail.com>
2024-07-17 12:10:40 +0200

    remove drop_last for val and test dataloader

--- experiment/LanguageDataModule.py
+++ experiment/LanguageDataModule.py
@@ -95,7 +95,6 @@ class LanguageDataModule(LightningDataModule):
             self.val_dataset,
             batch_size=self.args.eval_batch_size,
             collate_fn=self.collate_fn,
-            drop_last=True,
         )
 
     def test_dataloader(self) -> Optional[DataLoader]:
@@ -104,7 +103,6 @@ class LanguageDataModule(LightningDataModule):
                 self.test_dataset,
                 batch_size=self.args.eval_batch_size,
                 collate_fn=self.collate_fn,
-                drop_last=True,
             )
         return None
 
