18e2d47b07e42a38a4312b4bbfb6b19098969652
flaitenberger <f.laitenberger@gmail.com>
2024-07-22 13:36:38 +0200

    modified hippo init

--- experiment/utils/hippo_init.py
+++ experiment/utils/hippo_init.py
@@ -1,17 +1,22 @@
 import torch
 
 
-def hippo_init(A: torch.nn.Linear, B: torch.nn.Linear):
-    N = A.weight.shape[0]
-
-    q = torch.arange(N, dtype=torch.float64)
-    col, row = torch.meshgrid(q, q)
-
-    r = 2 * q + 1
-    M = -(torch.where(row >= col, r, 0) - torch.diag(q))
-    T = torch.sqrt(torch.diag(2 * q + 1))
-
-    with torch.no_grad():
-        A.weight.data = T @ M @ torch.linalg.inv(T)
-
+def hippo_init(A: torch.nn.Linear, B: torch.nn.Linear, method="LagT"):
+    if method == "LagT":
+        A.weight.data = torch.tril(torch.ones_like(A.weight.data))
         B.weight.data = torch.ones_like(B.weight.data)
+
+    else:
+        N = A.weight.shape[0]
+
+        q = torch.arange(N, dtype=torch.float64)
+        col, row = torch.meshgrid(q, q)
+
+        r = 2 * q + 1
+        M = -(torch.where(row >= col, r, 0) - torch.diag(q))
+        T = torch.sqrt(torch.diag(2 * q + 1))
+
+        with torch.no_grad():
+            A.weight.data = T @ M @ torch.linalg.inv(T)
+
+            B.weight.data = torch.ones_like(B.weight.data)
