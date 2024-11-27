ea02fbf3d01f1e9284e2703a5329535b1368e5f5
Thiggel <f.laitenberger@gmail.com>
2024-11-27 11:10:00 +0100

    custom inference bug fixes

---
+++ experiment/layers/NormalizedGemmaDecoderLayer.py
@@ -0,0 +1,255 @@
+from typing import Optional, Tuple
+import torch
+from torch import nn
+from transformers.cache_utils import Cache
+from transformers.models.gemma.modeling_gemma import (
+    GemmaConfig,
+    GemmaMLP,
+    GemmaRMSNorm,
+    GemmaAttention,
+    apply_rotary_pos_emb,
+    repeat_kv,
+)
+from transformers.activations import ACT2FN
+from transformers.ui import logging
+
+logger = logging.get_logger(__name__)
+
+
+class CanNormalize:
+    def normalize(self, x: torch.Tensor) -> torch.Tensor:
+        return x / x.norm(p=2, dim=-1, keepdim=True)
+
+
+class NormalizedGemmaMLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.hidden_size = config.hidden_size
+        self.intermediate_size = config.intermediate_size
+        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
+        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
+        self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=False)
+        if config.hidden_activation is None:
+            logger.warning_once(
+                "`config.hidden_act` is ignored, you should use `config.hidden_activation` instead.\n"
+                "Gemma's activation function will be set to `gelu_pytorch_tanh`. Please, use\n"
+                "`config.hidden_activation` if you want to override this behaviour.\n"
+                "See https://github.com/huggingface/transformers/pull/29402 for more details."
+            )
+            config.hidden_activation = "gelu_pytorch_tanh"
+        hidden_activation = config.hidden_activation
+        self.act_fn = ACT2FN[hidden_activation]
+
+        self.s_init_value = 1.0
+        self.s_init_scaling = 1.0
+        self.s = torch.nn.Parameter(
+            self.s_init_scaling * torch.ones(2 * 4 * config.hidden_size)
+        )
+        self.s_scaling = (self.s_init_value / self.s_init_scaling) * (
+            self.config.hidden_size**0.5
+        )
+
+    def forward(self, x):
+        gate = self.gate_proj(x)
+        up = self.up_proj(x)
+
+        s = self.s * self.s_scaling
+
+        gate = gate * s
+        up = up * s
+
+        output = self.act_fn(gate) * up
+
+        output = self.down_proj(output)
+
+        return output
+
+
+class NormalizedGemmaSdpaAttention(GemmaAttention, CanNormalize):
+    """
+    Gemma attention module using torch.nn.functional.scaled_dot_product_attention. This module inherits from
+    `GemmaAttention` as the weights of the module stays untouched. The only changes are on the forward pass to adapt to
+    SDPA API.
+    """
+
+    def __init__(self, config: GemmaConfig, layer_idx: int):
+        super().__init__(config, layer_idx)
+
+        self.sqk_init_value = 1.0
+        self.sqk_init_scaling = config.base_scale
+        self.sqk = torch.nn.Parameter(
+            self.sqk_init_scaling * torch.ones(self.config.n_embd, dtype=torch.float32)
+        )
+
+    # Adapted from GemmaAttention.forward
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Cache] = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+        cache_position: Optional[torch.LongTensor] = None,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+        if output_attentions:
+            # TODO: Improve this warning with e.g. `model.config.attn_implementation = "manual"` once this is implemented.
+            logger.warning_once(
+                "GemmaModel is using GemmaSdpaAttention, but `torch.nn.functional.scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to the manual attention implementation, "
+                'but specifying the manual implementation will be required from Transformers version v5.0.0 onwards. This warning can be removed using the argument `attn_implementation="eager"` when loading the model.'
+            )
+            return super().forward(
+                hidden_states=hidden_states,
+                attention_mask=attention_mask,
+                position_ids=position_ids,
+                past_key_value=past_key_value,
+                output_attentions=output_attentions,
+                use_cache=use_cache,
+                cache_position=cache_position,
+            )
+
+        bsz, q_len, _ = hidden_states.size()
+
+        query_states = self.q_proj(hidden_states)
+        key_states = self.k_proj(hidden_states)
+        value_states = self.v_proj(hidden_states)
+
+        query_states = query_states.view(
+            bsz, q_len, self.num_heads, self.head_dim
+        ).transpose(1, 2)
+        key_states = key_states.view(
+            bsz, q_len, self.num_key_value_heads, self.head_dim
+        ).transpose(1, 2)
+        value_states = value_states.view(
+            bsz, q_len, self.num_key_value_heads, self.head_dim
+        ).transpose(1, 2)
+
+        cos, sin = self.rotary_emb(value_states, position_ids)
+        query_states, key_states = apply_rotary_pos_emb(
+            query_states, key_states, cos, sin
+        )
+
+        sqk = (self.sqk * (self.sqk_init_value / self.sqk_init_scaling)).view(
+            1, 1, self.config.n_head, self.config.n_embd // self.config.n_head
+        )
+        query_states = sqk * self.normalize(query_states)
+        key_states = sqk * self.normalize(key_states)
+
+        if past_key_value is not None:
+            # sin and cos are specific to RoPE models; cache_position needed for the static cache
+            cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
+            key_states, value_states = past_key_value.update(
+                key_states, value_states, self.layer_idx, cache_kwargs
+            )
+
+        key_states = repeat_kv(key_states, self.num_key_value_groups)
+        value_states = repeat_kv(value_states, self.num_key_value_groups)
+
+        causal_mask = attention_mask
+        if attention_mask is not None:
+            causal_mask = causal_mask[:, :, :, : key_states.shape[-2]]
+
+        # SDPA with memory-efficient backend is currently (torch==2.1.2) bugged with non-contiguous inputs with custom attn_mask,
+        # Reference: https://github.com/pytorch/pytorch/issues/112577.
+        if query_states.device.type == "cuda" and causal_mask is not None:
+            query_states = query_states.contiguous()
+            key_states = key_states.contiguous()
+            value_states = value_states.contiguous()
+
+        # We dispatch to SDPA's Flash Attention or Efficient kernels via this `is_causal` if statement instead of an inline conditional assignment
+        # in SDPA to support both torch.compile's dynamic shapes and full graph options. An inline conditional prevents dynamic shapes from compiling.
+        is_causal = True if causal_mask is None and q_len > 1 else False
+
+        attn_output = torch.nn.functional.scaled_dot_product_attention(
+            query_states,
+            key_states,
+            value_states,
+            attn_mask=causal_mask,
+            dropout_p=self.attention_dropout if self.training else 0.0,
+            is_causal=is_causal,
+        )
+
+        attn_output = attn_output.transpose(1, 2).contiguous()
+        attn_output = attn_output.view(bsz, q_len, -1)
+
+        attn_output = self.o_proj(attn_output)
+
+        return attn_output, None, past_key_value
+
+
+class NormalizedGemmaDecoderLayer(nn.Module, CanNormalize):
+    def __init__(self, config: GemmaConfig, layer_idx: int):
+        super().__init__()
+        self.hidden_size = config.hidden_size
+
+        self.self_attn = NormalizedGemmaSdpaAttention(
+            config=config, layer_idx=layer_idx
+        )
+
+        self.mlp = GemmaMLP(config)
+        self.input_layernorm = GemmaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.post_attention_layernorm = GemmaRMSNorm(
+            config.hidden_size, eps=config.rms_norm_eps
+        )
+
+        self.attn_alpha_init_value = 0.05
+        self.attn_alpha_init_scaling = config.base_scale
+        self.attn_alpha = torch.nn.Parameter(
+            self.attn_alpha_init_scaling
+            * torch.ones(self.config.n_embd, dtype=torch.float32)
+        )
+
+        self.mlp_alpha_init_value = 0.05
+        self.mlp_alpha_init_scaling = config.base_scale
+        self.mlp_alpha = torch.nn.Parameter(
+            self.mlp_alpha_init_scaling
+            * torch.ones(self.config.n_embd, dtype=torch.float32)
+        )
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Cache] = None,
+        output_attentions: Optional[bool] = False,
+        use_cache: Optional[bool] = False,
+        cache_position: Optional[torch.LongTensor] = None,
+        **kwargs,
+    ) -> Tuple[
+        torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]
+    ]:
+        residual = hidden_states
+
+        # Self Attention
+        hidden_states, self_attn_weights, present_key_value = self.self_attn(
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_value=past_key_value,
+            output_attentions=output_attentions,
+            use_cache=use_cache,
+            cache_position=cache_position,
+        )
+        hidden_states = self.normalize(hidden_states)
+        hidden_states = self.normalize(
+            residual + self.attn_alpha * (hidden_states - residual)
+        )
+
+        # MLP
+        residual = hidden_states
+        hidden_states = self.normalize(self.mlp(hidden_states))
+        hidden_states = self.normalize(
+            residual + self.mlp_alpha * (hidden_states - residual)
+        )
+
+        outputs = (hidden_states,)
+
+        if output_attentions:
+            outputs += (self_attn_weights,)
+
+        if use_cache:
+            outputs += (present_key_value,)
+
+        return outputs
--- experiment/model_evaluator/CustomInference.py
+++ experiment/model_evaluator/CustomInference.py
@@ -17,16 +17,16 @@ class CustomInference:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def forward_only_transformer_layers(self, *args, **kwargs):
-        embed_tokens = self.model.get_input_embeddings()
-        lm_head = self.model.get_output_embeddings()
+        embed_tokens = self.model.model.get_input_embeddings()
+        lm_head = self.model.model.get_output_embeddings()
 
-        self.model.set_input_embeddings(nn.Identity())
-        self.model.set_output_embeddings(nn.Identity())
+        self.model.model.set_input_embeddings(nn.Identity())
+        self.model.model.set_output_embeddings(nn.Identity())
 
         output = self.model(*args, **kwargs).logits
 
-        self.model.set_input_embeddings(embed_tokens)
-        self.model.set_output_embeddings(lm_head)
+        self.model.model.set_input_embeddings(embed_tokens)
+        self.model.model.set_output_embeddings(lm_head)
 
         return output
 
@@ -56,12 +56,12 @@ class CustomInference:
         finished = torch.zeros(batch_size, dtype=torch.bool, device=self.device)
         import torch.nn.functional as F
 
-        embed_tokens = self.model.get_input_embeddings()(input_ids)
+        embed_tokens = self.model.model.get_input_embeddings()(input_ids)
         first_hidden_states = embed_tokens
         last_hidden_states = None
 
         for _ in range(max_length):
-            embed_tokens = self.model.get_input_embeddings()(input_ids)
+            embed_tokens = self.model.model.get_input_embeddings()(input_ids)
             first_hidden_states = embed_tokens
 
             hidden_states = self.forward_only_transformer_layers(first_hidden_states)
