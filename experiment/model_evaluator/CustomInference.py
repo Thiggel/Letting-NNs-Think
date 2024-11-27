5268298b1e1763eb4fee51184197e8ed048f9fff
Thiggel <f.laitenberger@gmail.com>
2024-11-27 11:16:41 +0100

    .

--- experiment/model_evaluator/CustomInference.py
+++ experiment/model_evaluator/CustomInference.py
@@ -61,23 +61,21 @@ class CustomInference:
         last_hidden_states = None
 
         for _ in range(max_length):
-            embed_tokens = self.model.model.get_input_embeddings()(input_ids)
-            first_hidden_states = embed_tokens
-
             hidden_states = self.forward_only_transformer_layers(first_hidden_states)
 
+            print(first_hidden_states.shape)
+            print(2, last_hidden_states.shape)
+            exit()
             if last_hidden_states is not None:
                 first_hidden_states[:, -1:, :] = last_hidden_states
-
-            hidden_states2 = self.forward_only_transformer_layers(first_hidden_states)
-            print(F.mse_loss(hidden_states[:, -1, :], hidden_states2[:, -1, :]))
+            print(first_hidden_states.shape)
 
             last_hidden_states = hidden_states[:, -1:, :]
             first_hidden_states = torch.cat(
                 (first_hidden_states, last_hidden_states), dim=1
             )
 
-            logits = self.model.model.lm_head(last_hidden_states)
+            logits = self.model.model.get_output_embeddings()(last_hidden_states)
             next_token_ids = torch.argmax(logits, dim=-1)
 
             input_ids = torch.cat((input_ids, next_token_ids), dim=1)
