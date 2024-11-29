5c0138f01ec970f7b6553ec40b1a1bf2dfb8403a
Thiggel <f.laitenberger@gmail.com>
2024-11-29 10:48:58 +0100

    .

--- experiment/layers/NormalizedGemmaDecoderLayer.py
+++ experiment/layers/NormalizedGemmaDecoderLayer.py
@@ -331,3 +331,19 @@ class NormalizedGemmaDecoderLayer(nn.Module, CanNormalize):
             outputs += (present_key_value,)
 
         return outputs
+
+
+class NormalizedGemmaLMHead(nn.Module):
+    def __init__(self, lm_head: nn.Linear):
+        super().__init__()
+        self.lm_head = lm_head
+        self.logit_scaling = nn.Parameter(torch.ones(lm_head.out_features))
+
+    @property
+    def weight(self):
+        return self.lm_head.weight
+
+    def forward(self, *args, **kwargs):
+        logits = self.lm_head(*args, **kwargs) * self.logit_scaling
+
+        return logits
