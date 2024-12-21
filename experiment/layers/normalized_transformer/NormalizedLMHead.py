bdc734f55d5c9a14ec6d3906a99cb4b3fed75a2d
Thiggel <f.laitenberger@gmail.com>
2024-12-21 11:31:11 +0100

    potential fix to lm head

--- experiment/layers/normalized_transformer/NormalizedLMHead.py
+++ experiment/layers/normalized_transformer/NormalizedLMHead.py
@@ -1,18 +1,32 @@
 import torch
 from torch import nn
+from torch.nn.parameter import Parameter
 
 
 class NormalizedLMHead(nn.Module):
     def __init__(self, lm_head: nn.Linear):
         super().__init__()
-        self.lm_head = lm_head
-        self.logit_scaling = nn.Parameter(torch.ones(lm_head.out_features))
+        # Instead of wrapping the lm_head, we'll create our own parameters
+        self.weight = Parameter(lm_head.weight.data.clone())
+        if hasattr(lm_head, "bias") and lm_head.bias is not None:
+            self.bias = Parameter(lm_head.bias.data.clone())
+        else:
+            self.register_parameter("bias", None)
 
-    @property
-    def weight(self):
-        return self.lm_head.weight
+        self.logit_scaling = Parameter(torch.ones(lm_head.out_features))
+        self.in_features = lm_head.in_features
+        self.out_features = lm_head.out_features
 
-    def forward(self, *args, **kwargs):
-        logits = self.lm_head(*args, **kwargs) * self.logit_scaling
+    def forward(self, hidden_states):
+        logits = nn.functional.linear(hidden_states, self.weight, self.bias)
+        return logits * self.logit_scaling
 
-        return logits
+    def _load_from_state_dict(self, state_dict, prefix, *args, **kwargs):
+        # If loading from a non-normalized model, handle the weight transfer
+        weight_key = prefix + "weight"
+        if weight_key in state_dict:
+            self.weight.data.copy_(state_dict[weight_key])
+            if self.bias is not None and prefix + "bias" in state_dict:
+                self.bias.data.copy_(state_dict[prefix + "bias"])
+
+        super()._load_from_state_dict(state_dict, prefix, *args, **kwargs)
